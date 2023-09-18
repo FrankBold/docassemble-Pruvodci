3c66da5b6cec427a3c36c9f0e09c8b195256509a
Michal Kuk <servis@frankbold.org>
2023-09-18 17:41:30 +0200

    test

--- docassemble/Pruvodci/extract_pdf.py
+++ docassemble/Pruvodci/extract_pdf.py
@@ -1,44 +1,37 @@
 import io
-import PyPDF2
 import requests
+import pikepdf
 from docassemble.base.util import path_and_mimetype, ocr_file, DAObject, DAFile
 
 def extract_pdf_pages(original, otazky, pages_to_remove, obec):
-  
-  pdf_file = open(original.path(), 'rb')
-  pdf_otazky = open(otazky.path(), 'rb')
-  
-  pdf_reader = PyPDF2.PdfFileReader(pdf_file)
-  otazky_reader = PyPDF2.PdfFileReader(pdf_otazky)
-  
-  pdf_writer = PyPDF2.PdfFileWriter()
+    # Open the input PDF files
+    pdf_file = pikepdf.open(original.path())
+    pdf_otazky = pikepdf.open(otazky.path())
+    
+    pdf_writer = pikepdf.Pdf.new()
+    
+    # Copy all pages except the ones specified for removal
+    for page_num, page in enumerate(pdf_file.pages):
+        if page_num not in pages_to_remove:
+            if page_num == 18:
+                for page_otazky in pdf_otazky.pages:
+                    pdf_writer.pages.append(page_otazky)
+            
+            pdf_writer.pages.append(page)
+    
+    edited_io = io.BytesIO()
+    
+    # Save the edited PDF to the BytesIO stream
+    pdf_writer.save(edited_io)
+    
+    # Move the cursor to the beginning of the BytesIO stream
+    edited_io.seek(0)
+    
+    files = {
+        'file': ('Analyza_' + obec + '.pdf', edited_io, 'application/pdf'),
+    }
 
-  # Copy all pages except the ones specified for removal
-  for page_num in range(pdf_reader.numPages):
-    if page_num not in pages_to_remove:
-      if page_num == 18:
-        for page_num_o in range(otazky_reader.numPages):
-          otazka = otazky_reader.getPage(page_num_o)
-          pdf_writer.addPage(otazka)
-        
-      page = pdf_reader.getPage(page_num)
-      page.mergePage(page)
-      pdf_writer.addPage(page)
-          
-  edited_io = io.BytesIO()
+    # Make a POST request to your specified URL
+    output = requests.post('https://hook.eu1.make.com/wske766bh4n63ym2389icfkshr8hsuik', files=files)
     
-  pdf_writer.write(edited_io)
-
-  pdf_file.close()
-  pdf_otazky.close()
-    
-  # Move the cursor to the beginning of the BytesIO stream
-  edited_io.seek(0)
-    
-  files = {
-  'file': ('Analyza_'+obec+'.pdf', edited_io, 'application/pdf'),
-  }
-
-  output = requests.post('https://hook.eu1.make.com/wske766bh4n63ym2389icfkshr8hsuik', files=files)
-    
-  return output.text
+    return output.text
