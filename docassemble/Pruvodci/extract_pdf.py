cf2e1aba985429f7b8bea65c33b6acf6734dd15c
Michal Kuk <servis@frankbold.org>
2023-09-18 17:52:26 +0200

    Update extract_pdf.py

--- docassemble/Pruvodci/extract_pdf.py
+++ docassemble/Pruvodci/extract_pdf.py
@@ -20,15 +20,9 @@ def extract_pdf_pages(original, otazky, pages_to_remove, obec):
             pdf_writer.pages.append(page)
 
     # Set the PDF to be read-only
-    pdf_writer.save('temp.pdf')
-    pdf_temp = pikepdf.open('temp.pdf')
-    pdf_temp.save('final.pdf', encryption=pikepdf.Encryption(owner='', user='', R=4, allow=pikepdf.Permissions.none))
-    
-    edited_io = io.BytesIO()
-    
-    # Save the edited PDF to the BytesIO stream
-    with open('final.pdf', 'rb') as f:
-        edited_io.write(f.read())
+    pdf_writer.save_to_stream()
+    pdf_temp = pikepdf.Pdf.open_stream(pdf_writer.stream())
+    pdf_temp.save_to_stream(encryption=pikepdf.Encryption(owner='', user='', R=4, allow=pikepdf.Permissions.none))
     
     edited_io = io.BytesIO()
     
